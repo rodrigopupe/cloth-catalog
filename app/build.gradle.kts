54af0ecdba01d4f919fe14199eff4d2dcd7424d8
Rodrigo Pupe <rodrigopupe@gmail.com>
2024-07-28 23:34:57 -0300

    Inclusão da dependência do RecyclerView.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
 
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
