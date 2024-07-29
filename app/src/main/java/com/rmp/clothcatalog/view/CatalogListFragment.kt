c0470b035f26ed01dd68518c2b4048f696e102ce
Rodrigo Pupe <rodrigopupe@gmail.com>
2024-07-29 20:18:48 -0300

    Implementação da recepção dos dados do backend + Renderização deles via Adapter.

--- app/src/main/java/com/rmp/clothcatalog/view/CatalogListFragment.kt
+++ app/src/main/java/com/rmp/clothcatalog/view/CatalogListFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.rmp.clothcatalog.databinding.FragmentCatalogListBinding
+import com.rmp.clothcatalog.utils.BaseState
+import com.rmp.clothcatalog.view.adapter.CatalogListAdapter
+import com.rmp.clothcatalog.view.model.ProductUIModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,11 +21,66 @@ class CatalogListFragment : Fragment() {
         FragmentCatalogListBinding.inflate(layoutInflater)
     }
 
+    private val catalogAdapter = CatalogListAdapter(::onProductClick)
+
+    private val viewModel: CatalogListViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        registerObservers()
+        setupView()
+
+        viewModel.getProductsList()
+    }
+
+    private fun registerObservers() {
+        viewModel.productsResponse.observe(viewLifecycleOwner, productsObserver)
+    }
+
+    private fun setupView() {
+        binding.rvProductsCatalog.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = catalogAdapter
+        }
+    }
+
+    private val productsObserver = Observer<BaseState<List<ProductUIModel>>> {
+        hideLoading()
+
+        when (it) {
+            is BaseState.Loading -> showLoading()
+
+            is BaseState.Success -> updateList(it.data)
+
+            is BaseState.Error -> {
+                // TODO Implementar
+            }
+        }
+    }
+
+    private fun updateList(list: List<ProductUIModel>) {
+        catalogAdapter.submitList(list)
+        binding.rvProductsCatalog.visibility = View.VISIBLE
+    }
+
+    private fun onProductClick(product: ProductUIModel) {
+        // TODO Implementar
+    }
+
+    private fun showLoading() {
+        binding.progressIndicator.visibility = View.VISIBLE
+    }
+
+    private fun hideLoading() {
+        binding.progressIndicator.visibility = View.GONE
     }
 }
--- app/src/main/java/com/rmp/clothcatalog/view/adapter/CatalogListAdapter.kt
+++ app/src/main/java/com/rmp/clothcatalog/view/adapter/CatalogListAdapter.kt
@@ -1,29 +1,47 @@
 package com.rmp.clothcatalog.view.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.rmp.clothcatalog.databinding.ItemProductBinding
+import com.rmp.clothcatalog.utils.setImageFromUrl
+import com.rmp.clothcatalog.utils.toCurrencyFormatted
+import com.rmp.clothcatalog.utils.toFormattedRating
 import com.rmp.clothcatalog.view.model.ProductUIModel
 
 class CatalogListAdapter(
     private val onItemClick: (ProductUIModel) -> Unit
 ) : ListAdapter<ProductUIModel, CatalogListAdapter.ViewHolder>(ProductsDiffCallback()) {
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): CatalogListAdapter.ViewHolder {
-        TODO("Not yet implemented")
+        val binding = ItemProductBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: CatalogListAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(currentList[position])
     }
 
     inner class ViewHolder(private val binding: ItemProductBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item: ProductUIModel) {
+            with(binding) {
+                ivProduct.setImageFromUrl(item.imageUrl)
+                tvProductName.text = item.title
+                tvProductPrice.text = item.price.toCurrencyFormatted()
+                tvProductRate.text = item.rating.rate.toFormattedRating()
 
+                root.setOnClickListener { onItemClick(item) }
+            }
         }
     }
 
@@ -36,6 +54,5 @@ class CatalogListAdapter(
         override fun areContentsTheSame(oldItem: ProductUIModel, newItem: ProductUIModel): Boolean {
             return oldItem == newItem
         }
-
     }
 }
