8b10a9df987cc39570e34c24b0a10a11eeb85c18
Rodrigo Pupe <rodrigopupe@gmail.com>
2024-07-28 21:24:30 -0300

    Criação do Módulo de Network para disponibilizar as instâncias do OkHttp e do Retrofit.

---
+++ app/src/main/java/com/rmp/clothcatalog/di/NetModule.kt
@@ -0,0 +1,41 @@
+package com.rmp.clothcatalog.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+private const val TIMEOUT_MILLISECONDS = 30000L
+private const val BASE_URL = "https://fakestoreapi.com"
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetModule {
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        return OkHttpClient.Builder()
+            .connectTimeout(TIMEOUT_MILLISECONDS, TimeUnit.MILLISECONDS)
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
