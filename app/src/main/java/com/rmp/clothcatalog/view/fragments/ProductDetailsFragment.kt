1c1850c0882b80d3ca082bf3d3956f616fe7fe3c
Rodrigo Pupe <rodrigopupe@gmail.com>
2024-07-29 23:03:39 -0300

    Renderização dos dados dos detalhes do Produto selecionado.

--- app/src/main/java/com/rmp/clothcatalog/view/fragments/ProductDetailsFragment.kt
+++ app/src/main/java/com/rmp/clothcatalog/view/fragments/ProductDetailsFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.rmp.clothcatalog.R
+import androidx.navigation.fragment.navArgs
 import com.rmp.clothcatalog.databinding.FragmentProductDetailsBinding
+import com.rmp.clothcatalog.utils.setImageFromUrl
+import com.rmp.clothcatalog.utils.toCurrencyFormatted
+import com.rmp.clothcatalog.utils.toFormattedRating
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,11 +19,32 @@ class ProductDetailsFragment : Fragment() {
         FragmentProductDetailsBinding.inflate(layoutInflater)
     }
 
+    private val args: ProductDetailsFragmentArgs by navArgs()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        with(args.product) {
+            binding.apply {
+                tvProductName.text = title
+                ivProduct.setImageFromUrl(imageUrl)
+                tvProductDescription.text = description
+                tvCategory.text = category
+                tvPrice.text = price.toCurrencyFormatted()
+                tvProductRate.text = rating.rate.toFormattedRating()
+            }
+        }
     }
 }
